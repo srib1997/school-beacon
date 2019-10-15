77e85f06d2116326a565b8a0455b46d993fe3a8a
srib <peterf1a08@yahoo.com.hk>
2019-10-15 17:59:29 +0800

    刪掉多餘的 code

--- App/screen/home/index.js
+++ App/screen/home/index.js
@@ -57,51 +57,6 @@ class Home extends Component {
           }
           keyExtractor={item => `homepage${item.name}`}
         />
-        {/* 資料 */}
-        {/* <ListItem thumbnail onPress={() => this.goToInfo(datas.first, '地下')}>
-          <Left>
-            <Thumbnail square source={{ uri: datas.first[0].img }} />
-          </Left>
-          <Body>
-            <Text>
-              地下
-            </Text>
-            <Text numberOfLines={1} note>
-              是同學們經常休息的地方
-            </Text>
-          </Body>
-          <Right />
-        </ListItem>
-
-        <ListItem thumbnail onPress={() => this.goToInfo(datas.second, '第一層')}>
-          <Left>
-            <Thumbnail square source={{ uri: datas.second[0].img }} />
-          </Left>
-          <Body>
-            <Text>
-              第一層
-            </Text>
-            <Text numberOfLines={1} note>
-              是同學們上堂的地方
-            </Text>
-          </Body>
-          <Right />
-        </ListItem>
-
-        <ListItem thumbnail onPress={() => this.goToInfo(datas.third, '第二層')}>
-          <Left>
-            <Thumbnail square source={{ uri: datas.third[0].img }} />
-          </Left>
-          <Body>
-            <Text>
-              第二層
-            </Text>
-            <Text numberOfLines={1} note>
-              是同學們上堂的地方
-            </Text>
-          </Body>
-          <Right />
-        </ListItem> */}
 
       </Container>
     )
